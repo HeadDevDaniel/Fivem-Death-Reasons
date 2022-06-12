70f17ce86ac8d2de702ee1518ab4103869f31ed6
Daniel <84569646+HeadDevDaniel@users.noreply.github.com>
2022-06-12 23:33:42 +1200

    Create fxmanifest.lua

---
+++ fxmanifest.lua
@@ -0,0 +1,13 @@
+fx_version 'cerulean'
+game { 'gta5' }
+author 'HeadDevDaniel'
+description 'Main Scripts'
+version '1.0.0'
+
+client_scripts {
+    'client.lua'
+}
+
+server_scripts {
+    'server.lua'
+}
